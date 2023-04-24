04adda362278e0578702acd5669484c03b798a42
Neerav Garg <neeravgarg366@gmail.com>
2023-04-24 18:42:22 +0530

    mayways test form

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Form from './form'
+export default function App() {
+
+
+  return (
+    <main>
+      <Form />
+    </main>
+  )
+}
---
+++ src/form.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {useState} from "react"
+
+const Form = ( )=>{
+const [name, setName]   = useState("");
+const [email, setEmail]   = useState("");
+const [age, setAge]   = useState("");
+
+const handleSubmit =(e)=>{
+ e.preventdefault()
+  if(name && email && age )
+    alert("Voila, you did it ${name}! Your age is ${email} and your email is ${age}.")
+  else
+    alert("Please fill proper info")
+    
+}
+  
+  return (
+    <>
+      <form onSubmit ={handleSubmit} >
+       <label>
+         Name
+       </label>
+       <input type="text" minLength={4} onChange = {(e)=>setName(e.target.value)}/ >
+       <label>
+         Email
+       </label>
+       <input type="email" minLength={4} onChange = {(e)=>setName(e.target.value)}/ >
+       <label>
+         Age
+       </label>
+       <input type="number"  onChange = {(e)=>{
+      if(e.target.value >=18)setName(e.target.value)
+    } }/ >
+      <input type="submit"></input>
+      
+     </form>
+    </>
+  )
+}
+
+export default Form
