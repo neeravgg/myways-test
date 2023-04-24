83f17f43f32137b0006ca5c038e5fa6f7fdf9f8e
Neerav Garg <neeravgarg366@gmail.com>
2023-04-24 22:57:12 +0530

    mayways test form

--- src/form.jsx
+++ src/form.jsx
@@ -1,43 +1,46 @@
 import React from "react";
-import {useState} from "react"
+import { useState } from "react";
 
-const Form = ( )=>{
-const [name, setName]   = useState("");
-const [email, setEmail]   = useState("");
-const [age, setAge]   = useState("");
+const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
 
-const handleSubmit =(e)=>{
- e.preventdefault()
-  if(name && email && age )
- 
-    window.alert("Voila, you did it ${name}! Your age is ${email} and your email is ${age}.")
-  else
-    window.alert("Please fill proper info")
-    
-}
-  
+  const handleSubmit = (e) => {
+    e.preventdefault();
+    if (name && email && age)
+      alert(
+        "Voila, you did it ${name}! Your age is ${email} and your email is ${age}."
+      );
+    else alert("Please fill proper info");
+  };
+
   return (
     <>
-      <form onSubmit ={handleSubmit} >
-       <label>
-         Name
-       </label>
-       <input type="text" minLength={4} onChange = {(e)=>setName(e.target.value)}/ >
-       <label>
-         Email
-       </label>
-       <input type="email" minLength={4} onChange = {(e)=>setName(e.target.value)}/ >
-       <label>
-         Age
-       </label>
-       <input type="number"  onChange = {(e)=>{
-      if(e.target.value >=18)setName(e.target.value)
-    } }/ >
-      <input type="submit"></input>
-      
-     </form>
+      <form onSubmit={handleSubmit}>
+        <label>Name</label>
+        <input
+          type='text'
+          minLength={4}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>Email</label>
+        <input
+          type='email'
+          minLength={4}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Age</label>
+        <input
+          type='number'
+          onChange={(e) => {
+            if (e.target.value >= 18) setAge(e.target.value);
+          }}
+        />
+        <input type='submit'></input>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
